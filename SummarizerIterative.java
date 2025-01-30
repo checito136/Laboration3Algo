6da0fc800d74cb7fcda4d8811cebfdacffebf283
Ernesto Vidal <ernesto.vidal.1365@student.uu.se>
2025-01-30 13:50:25 +0100

    nothing new

---
+++ SummarizerIterative.java
@@ -0,0 +1,16 @@
+package algo.sum;
+/**
+ * Algorithmic methods to calculate sum using iterative solutions.
+ */
+public class SummarizerIterative implements Summarizer {
+	@Override
+	public long sumOfNaturalNumbers(long n) {
+		// TODO Implement an iterative solution.
+		return 0;
+	}
+	@Override
+	public long sumOfNaturalNumbersSquared(long n) {
+		// TODO Implement an iterative solution.
+		return 0;
+	}
+}
---
+++ SummarizerRecursive.java
@@ -0,0 +1,16 @@
+package algo.sum;
+/**
+ * Algorithmic methods to calculate sum using recursive solutions.
+ */
+public class SummarizerRecursive implements Summarizer {
+	@Override
+	public long sumOfNaturalNumbers(long n) {
+		// TODO Implement a recursive solution.
+		return 0;
+	}
+	@Override
+	public long sumOfNaturalNumbersSquared(long n) {
+		// TODO Implement a recursive solution.
+		return 0;
+	}
+}
