ddde65669a47ff945347a3e5fd5c2f2937df435c
Ernesto Vidal <ernesto.vidal.1365@student.uu.se>
2025-02-10 21:59:33 +0100

    Did the todo tasks

--- SummarizerIterative.java
+++ SummarizerIterative.java
@@ -6,11 +6,22 @@ public class SummarizerIterative implements Summarizer {
 	@Override
 	public long sumOfNaturalNumbers(long n) {
 		// TODO Implement an iterative solution.
-		return 0;
+
+		long sum = 0;
+
+		for (long i = 0; i >= 1; i++) {
+			sum += i;
+		}
+		return sum;
 	}
 	@Override
 	public long sumOfNaturalNumbersSquared(long n) {
 		// TODO Implement an iterative solution.
-		return 0;
+
+		long sum = 0;
+		for (long i = 1; i <= n; i++) {
+			sum += i * i;
+		}
+		return sum;
 	}
 }
--- SummarizerRecursive.java
+++ SummarizerRecursive.java
@@ -6,11 +6,17 @@ public class SummarizerRecursive implements Summarizer {
 	@Override
 	public long sumOfNaturalNumbers(long n) {
 		// TODO Implement a recursive solution.
+		if (n > 0) {
+			return n + sumOfNaturalNumbers(n - 1);
+		}
 		return 0;
 	}
 	@Override
 	public long sumOfNaturalNumbersSquared(long n) {
 		// TODO Implement a recursive solution.
+		if (n > 0) {
+			return n * n + sumOfNaturalNumbersSquared(n - 1);
+		}
 		return 0;
 	}
 }
